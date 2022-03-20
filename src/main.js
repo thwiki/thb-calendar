45cad05018fe579bdb56a0e4c129ec491a51a47e
shinki <shinki@do-core.cn>
2022-03-20 22:09:59 +0800

    妈妈再也不用看jquery的脸色

--- src/main.js
+++ src/main.js
@@ -4,34 +4,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-// RLQ是MediaWiki保存异步执行函数的数组
-if (process.env.NODE_ENV === 'production') {
-  window.RLQ = window.RLQ || [];
-  window.RLQ.push(() => {
-    // 等待jQuery加载完毕
-    var _count = 0;
-    var _interval = setInterval(() => {
-      _count++;
-      if (typeof jQuery !== "undefined") {
-        // jQuery加载完毕
-        clearInterval(_interval);
-        // 防止网站并不是MediaWiki时报错
-        try {
-          new Vue({
-            render: h => h(App),
-          }).$mount('.app-calendar');
-        } catch (err) {
-          console.error(err);
-        }
-      } else if (_count > 30 * 5) {
-        // 加载超时
-        clearInterval(_interval);
-      }
-    }, 200);
-  });
-}
-else {
-  new Vue({
-    render: h => h(App),
-  }).$mount('.app-calendar');
-}
+new Vue({
+  render: h => h(App),
+}).$mount('.app-calendar');
